bbf43d09a01079483f1c6c538550ddc96126a4a1
hj.kim2 <hj.kim2@alticast.com>
2021-11-10 18:53:04 +0900

    추가 수정 사항 적용

--- src/core/components/Component.ts
+++ src/core/components/Component.ts
@@ -1,17 +1,25 @@
-export type ComponentTypes = "div" | "span";
+export type ComponentTypes = "div" | "span" | "ul" | "li" | "header";
 
-export default abstract class Component<IComponentState = {}, IComponentProps = {}> {
-  private container: JQuery;
-  private renderTemplate: string;
-  private state: IComponentState;
+export default abstract class Component<
+  TComponentState = {},
+  TComponentProps = {}
+> {
+  private container: HTMLElement;
+  private renderTemplate: string = "";
+  private state: TComponentState = {} as TComponentState;
+  private props: TComponentProps;
+  private parents: HTMLElement | null = null;
 
-  constructor(
-    type: ComponentTypes,
-    props: IComponentProps,
-    children: Component[]
-  ) {
-    this.container = $(`<${type}>`);
+  constructor(type: ComponentTypes = "div", props: TComponentProps, parents: HTMLElement | null = null) {
+    this.props = props;
+    this.parents = parents;
+    this.container = document.createElement(type);
   }
+  private async lifeCycle() {
+    await this.setState();
+    //this.state = observalbe<TComponentState>(this.state);
+  }
+  protected abstract setState(): Promise<TComponentState> | null;
   resume(): void {}
   show(): void {}
   pause(): void {}
@@ -21,7 +29,7 @@ export default abstract class Component<IComponentState = {}, IComponentProps = 
   abstract onKeyEvent(evt: KeyboardEvent): boolean;
   render(...params: string[]): void {
     this.renderTemplate = this.template();
-    this.container.html(this.renderTemplate);
+    this.container.innerHTML = this.renderTemplate;
     this.mounted();
   }
 }
---
+++ src/core/utils/CommonUtils.ts
@@ -0,0 +1,16 @@
+/**
+ * class extends 에서는 다중 상속이 되지 않으므로 mixin을 통해 다중 상속을 지원한다.
+ * @param targetClass
+ * @param baseClasses
+ */
+function applyMixins(targetClass: any, baseClasses: any[]): void {
+  baseClasses.forEach((baseClass) => {
+    Object.getOwnPropertyNames(baseClass.prototype).forEach((name) => {
+      Object.defineProperty(
+        targetClass.proptotype,
+        name,
+        Object.getOwnPropertyDescriptor(baseClass.prototype, name) || Object.create(null)
+      );
+    });
+  });
+}
